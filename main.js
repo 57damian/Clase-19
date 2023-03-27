507873951ff021e380c34cb646e69d6961b18138
57damian <campbelldamian57@gmail.com>
2023-03-27 16:50:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+const express = require(`express`);
+const { Server: HttpServer } = require(`http`);
+const { Server: IOServer } = require(`socket.io`);
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+app.use(express.static(`./public`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set(`views`, `./views`);
+app.set(`view engine`, `ejs`);
+
+const PORT = 8081;
+httpServer.listen(PORT, () => console.log(`Servidor escuchando el puerto ${PORT}`));
+
+let users = [];
+
+//CRUD db
+const { selectAllProducts } = require(`./db/selectAllProducts`);
+const { insertProduct } = require(`./db/insertProduct`);
+const { selectAllMessage } = require(`./db/selectAllMessage`);
+const { insertMessage } = require(`./db/insertMessage`);
+
+//Routers import
+const homeRouter = require(`./routers/homeRouter`);
+const formRouter = require(`./routers/formRouter`);
+const loginRouterGet = require(`./routers/loginRouterGet`);
+const loginRouterPost = require(`./routers/loginRouterPost`);
+const chatRouter = require(`./routers/chatRouter`);
+
+//Routers
+app.use(`/`, homeRouter);
+app.use(`/form`, formRouter);
+app.use(`/login`, loginRouterGet);
+app.use(`/login`, loginRouterPost);
+
+
+//socket Products
+io.on(`connection`, socket => {
+    socket.on(`sendProduct`, async () => {
+        try {
+            const allProductsFromDB = await selectAllProducts();
+
+            //Servidor --> Cliente : Se envian todos los mensajes al usuario que se conectó.
+            socket.emit(`allProducts`, allProductsFromDB);
+        } catch (err) {
+            console.log(`Error ${err}`);
+        }
+    });
+
+    socket.on(`addProducts`, async data => {
+        try {
+            const newProducto = {
+                title: `${data.name}`,
+                price: Number(data.price),
+                thumbnail: `${data.img}`
+            };
+
+            const product = await insertProduct(newProducto);
+
+            //Envio el producto nuevo a todos los clientes conectados
+            io.sockets.emit(`refreshTable`, [product]);
+
+        } catch (err) {
+            console.log(`Error ${err}`);
+        }
+    });
+});
+
